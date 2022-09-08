d0357f6e3bf24921e928ededec3908d6bd6cb496
Dario Busacca <dario_999@hotmail.it>
2022-09-08 19:38:41 +0200

    video emotion recognition done

--- face_detector.py
+++ face_detector.py
@@ -28,20 +28,61 @@ def getLastFrame(videofile):
     if(success):
         cv2.imwrite("last_frame.jpg", image)
     
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
     
+# similar to run.py file
 def emotionDetection(imageFile):
-    #TODO
-    return
+    cut_size = 44
+
+    transform_test = transforms.Compose([
+        transforms.TenCrop(cut_size),
+        transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+    ])
+    
+    raw_img = io.imread(imageFile)
+    gray = rgb2gray(raw_img)
+    gray = resize(gray, (48, 48), mode = 'symmetric').astype(np.uint8)
+    
+    img = gray[:, :, np.newaxis]
+
+    img = np.concatenate((img, img, img), axis=2)
+    img = Image.fromarray(img)
+    inputs = transform_test(img)
+    
+    class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+    net = VGG('VGG16')
+    checkpoint = torch.load(os.path.join('FER2013_VGG19', 'PrivateTest_model.t7'))
+    net.load_state_dict(checkpoint['net'])
+    net.cuda()
+    net.eval()
+
+    ncrops, c, h, w = np.shape(inputs)
+
+    inputs = inputs.view(-1, c, h, w)
+    inputs = inputs.cuda()
+    inputs = Variable(inputs, volatile = True)
+    outputs = net(inputs)
+    
+    outputs_avg = outputs.view(ncrops, -1).mean(0)  #avg crops
+    
+    score = F.softmax(outputs_avg)
+    _, predicted = torch.max(outputs_avg.data, 0)
+    
+    return str(class_names[int(predicted.cpu().numpy())])
 
 
 # Load the cascade  
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')  
   
 # To capture video from existing video.   
-cap = cv2.VideoCapture('test.mp4')
-getFirstFrame('test.mp4')
-getLastFrame('test.mp4')
-    
+cap = cv2.VideoCapture('video/alessia.mp4')
+getFirstFrame('video/alessia.mp4')
+getLastFrame('video/alessia.mp4')
+
+print(emotionDetection('first_frame.jpg'))
+print(emotionDetection('last_frame.jpg'))
 
 if(cap.isOpened() == False):
     print("Error opening video for stream")
